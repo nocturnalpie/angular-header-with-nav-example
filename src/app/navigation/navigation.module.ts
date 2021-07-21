5d1e2be2c8f7edf0a111893c6f0999d4fe0fdb79
nocturnalpie <nicole@theliveseys.net>
2021-07-21 11:25:51 +0100

    made it work in stackblitz

--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -1,4 +1,3 @@
-import { LayoutModule } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,26 +5,21 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { AppRoutingModule } from 'src/app/app-routing.module';
+import { AppRoutingModule } from '../app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './navigation.component';
 
 @NgModule({
-	declarations: [
-		NavigationComponent,
-		HeaderComponent,
-	],
-	imports: [
-		CommonModule,
-		AppRoutingModule,
-		MatToolbarModule,
-		MatButtonModule,
-		MatSidenavModule,
-		MatIconModule,
-		MatListModule
-	],
-	exports: [
-		NavigationComponent
-	]
+  declarations: [NavigationComponent, HeaderComponent],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  exports: [NavigationComponent]
 })
-export class NavigationModule { }
+export class NavigationModule {}
